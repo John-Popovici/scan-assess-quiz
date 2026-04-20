a4b7079a9bac4e298a4044c1a3ed4b62066bd67d
draken <thedraken@gmail.com>
2026-04-20 21:40:12 +0200

    Creating subfolder for python and java project

---
+++ Java/QuizSelectorGame/src/main/java/org/uni/lu/quizselectorgame/QuizSelectorGameApplication.java
@@ -0,0 +1,13 @@
+package org.uni.lu.quizselectorgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSelectorGameApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(QuizSelectorGameApplication.class, args);
+    }
+
+}
---
+++ Java/QuizSelectorGame/src/test/java/org/uni/lu/quizselectorgame/QuizSelectorGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uni.lu.quizselectorgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSelectorGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
