8d57ec7634fe48024bbe4f850cc8e4e9d0004e0b
draken <thedraken@gmail.com>
2026-04-26 18:07:41 +0200

    Initial home page creation Default layout with questions and options to pick

--- Java/QuizSelectorGame/src/main/java/org/uni/lu/quizselectorgame/MainView.java
+++ Java/QuizSelectorGame/src/main/java/org/uni/lu/quizselectorgame/MainView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.card.Card;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,13 +12,24 @@ import com.vaadin.flow.router.Route;
 @Route
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private final SecurityScore securityScore = new SecurityScore();
 
+    public MainView() {
+        setAlignItems(Alignment.CENTER);
 
         add(new Text("You have a choice, do you go for option 1 or 2?"));
-        Button leftButton = new Button("Select option 1", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionOne());
-        Button rightButton = new Button("Select option 2", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionTwo());
-        HorizontalLayout buttonLayout = new HorizontalLayout(leftButton, rightButton);
+        Card leftCard = new Card();
+        Card rightCard = new Card();
+
+        Button leftButton = new Button("<-", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionOne());
+        Button rightButton = new Button("->", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionTwo());
+
+        leftCard.setTitle("Option 1");
+        leftCard.add(leftButton);
+        rightCard.setTitle("Option 2");
+        rightCard.add(rightButton);
+
+        HorizontalLayout buttonLayout = new HorizontalLayout(leftCard, rightCard);
         add(buttonLayout);
     }
 
