fa75edf550eb0a0ea73c10456b47676413167d87
draken <thedraken@gmail.com>
2026-04-26 17:57:13 +0200

    Initial home page creation Default layout with questions and options to pick

--- Java/QuizSelectorGame/src/main/java/org/uni/lu/quizselectorgame/MainView.java
+++ Java/QuizSelectorGame/src/main/java/org/uni/lu/quizselectorgame/MainView.java
@@ -1,6 +1,10 @@
 package org.uni.lu.quizselectorgame;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -8,6 +12,20 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new Text("Home page"));
+
+
+        add(new Text("You have a choice, do you go for option 1 or 2?"));
+        Button leftButton = new Button("Select option 1", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionOne());
+        Button rightButton = new Button("Select option 2", (ComponentEventListener<ClickEvent<Button>>) _ -> choseOptionTwo());
+        HorizontalLayout buttonLayout = new HorizontalLayout(leftButton, rightButton);
+        add(buttonLayout);
+    }
+
+    private void choseOptionOne() {
+
+    }
+
+    private void choseOptionTwo() {
+
     }
 }
